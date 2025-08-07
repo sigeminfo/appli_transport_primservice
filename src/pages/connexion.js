b0f3c7790e0b7891ef65bd83b152949908d40bf7
Mattéo <43877671+MatteoLB@users.noreply.github.com>
2025-08-07 13:16:38 +0200

    logo jpg

--- src/pages/connexion.js
+++ src/pages/connexion.js
@@ -42,7 +42,7 @@ export class ConnexionPage extends HTMLElement {
             <div class='p-3'>
                 <div class='bg-white px-3 py-7 flex flex-col gap-3 rounded-md'>
                     <div class='flex justify-center mb-3'>
-                        <img class='h-20' src='/img/logos/logoclient.svg'>
+                        <img class='h-20' src='/img/logos/logoclient.jpg'>
                     </div>
                     <h1 class='font-bold'>Transport Emballages</h1>
                     <div>
